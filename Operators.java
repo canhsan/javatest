7330a7777b0c9e42a440e5742bfb28cbbc2b5a13
canhsan <1979hasancan@gmail.com>
2022-12-25 21:27:52 +0300

    operator sonuc

--- Operators.java
+++ Operators.java
@@ -1,6 +1,9 @@
 public class Operators {
  public static void main(String[] args) {
-     int a=4;
+     int a=8;
+     int b=4;
+             int sonuc=a/b;
+     System.out.println(sonuc);
      System.out.println("selam mesudum naber kanki");
     }
 }
